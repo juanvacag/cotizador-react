b0b64a4cbbb0942765cac67fbbe7cfdd0bf8066c
juanvacag <>
2022-06-23 20:18:53 +0200

    final

--- src/components/AppSeguro.jsx
+++ src/components/AppSeguro.jsx
@@ -9,7 +9,7 @@ const AppSeguro = () => {
   return (
     <>
         <header className="my-10">
-            <h1 className="text-white text-center text-4xl font-black">Cotizador de Seguros de Coches</h1>
+            <h1 className="text-white text-center text-4xl font-black">Calcular el Seguro del Coche</h1>
         </header>
 
         <main className="bg-white md:w-2/3 lg:w-2/4 mx-auto shadow rounded-lg p-10">
--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,13 +1,15 @@
+import { useCallback, useMemo, useRef } from "react"
 import useCotizador from "../hooks/useCotizador"
-import {MARCAS, PLANES} from '../constants'
+import {MARCAS, PLANES} from "../constants"
 
 const Resultado = () => {
 
     const {resultado, datos} = useCotizador()
     const {marca, year, plan} = datos
+    const yearRef = useRef(year)
 
-    const [nombreMarca] = MARCAS.filter(m => m.id === Number(marca))
-    const [nombrePlan] = PLANES.filter(p => p.id === Number(plan))
+    const [nombreMarca] = useCallback(MARCAS.filter(m => m.id === Number(marca)), [resultado])
+    const [nombrePlan] = useCallback(PLANES.filter(p => p.id === Number(plan)),[resultado])
     
     if(resultado === 0) return null
 
@@ -15,7 +17,7 @@ const Resultado = () => {
         <div className="bg-gray-100 text-center mt-5 p-5 shadow">
             <h2 className="text-gray-600 font-black text-3xl">Resumen</h2>
             <p className="my-2"><span className="font-bold">Origen: </span>{nombreMarca.nombre}</p>
-            <p className="my-2"><span className="font-bold">Matriculación: </span>{year}</p>
+            <p className="my-2"><span className="font-bold">Matriculación: </span>{yearRef.current}</p>
             <p className="my-2"><span className="font-bold">Plan: </span>{nombrePlan.nombre}</p>
             <p className="my-2 text-2xl"><span className="font-bold">Total Seguro: </span>{resultado}</p>
         </div>
